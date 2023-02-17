1974faecd741626aa72e225a193b05e5413c4f68
Martin Burolla <mburolla@xpanxion.com>
2023-02-17 10:32:05 -0500

    update

--- src/main/java/Model/CarRecord.java
+++ src/main/java/Model/CarRecord.java
@@ -1,6 +1,20 @@
 package Model;
 
 public class CarRecord {
+    private Integer Id;
+    private String make;
+    private String model;
+    private String price;
 
+    public CarRecord(Integer id, String make, String model, String price) {
+        Id = id;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
 
+    @Override
+    public String toString() {
+        return "{" + Id + " " + make + " " + model + " " + price + "}";
+    }
 }
--- src/main/java/Model/EngineRecord.java
+++ src/main/java/Model/EngineRecord.java
@@ -1,4 +1,21 @@
 package Model;
 
+import java.text.NumberFormat;
+
 public class EngineRecord {
+    private Integer numCylinders;
+    private Float displacment;
+    private Float taxes;
+
+    public EngineRecord(Integer numCylinders, Float displacment, Float taxes) {
+        this.numCylinders = numCylinders;
+        this.displacment = displacment;
+        this.taxes = taxes;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return "{" + numCylinders + " " + displacment + " " + formatter.format(taxes) + "}";
+    }
 }
