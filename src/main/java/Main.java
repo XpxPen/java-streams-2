47e59b683e713ce7e906714bb281efceea2e4c17
Josue Gonzalez <josue.gonzalez@penske.com>
2023-02-20 17:02:31 -0500

    ex4 to ex10 left

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import Model.CarRecord;
 import Model.EngineRecord;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Locale;
 import java.text.NumberFormat;
 import java.util.stream.IntStream;
@@ -11,19 +12,46 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+
+        //ex1();
+        //ex2();
+        ex3();
+        //ex4();
+        //ex5();
+        //ex6();
+        //ex7();
+        //ex8();
+        //ex9();
+        //ex10();
     }
 
     private static void ex1() {
-        // TODO...
+        var peopleList = DataRepo.getPeople();
+        var retList = peopleList.stream()
+                .filter(r -> r.getAge() > 30)
+                .collect(Collectors.toList());
+
+        System.out.println(retList);
     }
 
     private static void ex2() {
-        // TODO...
+        var peopleList = DataRepo.getPeople();
+        var retList = peopleList.stream()
+                .mapToInt(r -> r.getAge())
+                .average()
+                .getAsDouble();
+
+        System.out.println(retList);
     }
 
     private static void ex3() {
-        // TODO...
+        var catList = DataRepo.getCats();
+        var retList = catList.stream()
+                .filter(r -> r.getColor() == "Brown")
+                .sorted(Comparator.comparing(r -> r.getAge()))
+                .collect(Collectors.toList());
+
+        System.out.println(retList);
     }
 
     private static void ex4() {
